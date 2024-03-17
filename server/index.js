1158e67b4d8d0b4cfd666dc77d577be96701e6e1
vinsst <vinsstcompany@gmail.com>
2024-03-17 12:40:16 +0300

    fghsdghjdfsghjkdfsghjkdfs

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const port = 3001;
+
+const dir_rootApp = path.dirname(__dirname);
+
+function printDirectoryStructure(directoryPath, indent = "") {
+  const items = fs.readdirSync(directoryPath);
+  items.forEach((item) => {
+    const itemPath = path.join(directoryPath, item);
+    if (item === "node_modules" || item === ".git") {
+      return;
+    }
+    const stats = fs.statSync(itemPath);
+    if (items.length > 15) {
+      console.log(indent + "📁 " + item);
+      return;
+    }
+    if (item === "build" || item === "assets") {
+      console.log(indent + "📁 " + item);
+      return;
+    }
+    if (stats.isDirectory()) {
+      console.log(indent + "📁 " + item);
+      printDirectoryStructure(itemPath, indent + "  ");
+    } else {
+      console.log(indent + "📄 " + item);
+    }
+  });
+}
+
+const projectRootPath = dir_rootApp;
+
+console.log("📁 Project Structure:");
+printDirectoryStructure(projectRootPath);
+
+console.log(path.join(dir_rootApp, "/clientGoogleReview/build"));
+console.log(__dirname);
+
+app.listen(port, () => {
+  console.log("Listening, port " + port);
+});
+
+app.use(express.json());
